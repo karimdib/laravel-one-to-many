37bfa850aad89e67a8edba3e6e7c77764a26801d
karimdib <karim.dib2001@gmail.com>
2024-01-09 17:05:53 +0100

    fix-create-edit

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -13,6 +13,15 @@
                 <label for="description" class="mb-2">description</label>
                 <input type="text" class="form-control" id="description" placeholder="description" name="description">
             </div>
+            <div class="form-group p-3">
+                <label for="">scgli il tipo</label>
+                <select name="type_id" class="form-control" id="type_id">
+                    <option value="" disabled='disabled'>Scegli un tipo</option>
+                    @foreach($types as $type)
+                    <option>{{$type->name }}</option>
+                    @endforeach
+                </select>
+            </div>
             <button type="submit" class="btn btn-primary m-3">Submit</button>
         </form>
     </div>
